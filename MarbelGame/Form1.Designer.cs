634466252adc9b11f9167cab3c6dca04e892e6e0
Luiz DuPont <91236027+izzdupont@users.noreply.github.com>
2022-11-17 10:08:50 -0600

    Marble Game

---
+++ MarbelGame/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace MarbelGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stopWatch1 = new MarbelGame_Dll.StopWatch();
+            this.SuspendLayout();
+            // 
+            // stopWatch1
+            // 
+            this.stopWatch1.Location = new System.Drawing.Point(625, 210);
+            this.stopWatch1.Name = "stopWatch1";
+            this.stopWatch1.Size = new System.Drawing.Size(150, 150);
+            this.stopWatch1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopWatch1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MarbelGame_Dll.StopWatch stopWatch1;
+    }
+}
+
